a71082434b0c61e5b0e6b02fd0a266dca3478de5
Dexshine <b_o_y_s@hotmail.com>
2021-01-12 13:58:54 +0700

    button create table

--- app_template.js
+++ app_template.js
@@ -1,5 +1,5 @@
 //your code here!
-document.addEventListener('DOMContentloaded',modifyId);
+document.addEventListener('DOMContentloaded',createTable);
 let inputNum = document.getElementById("input-number");
 let inputButton1 = document.getElementById("button-compute");
 let outputResult1 = document.getElementById("compute-result");
@@ -7,9 +7,17 @@ let inputRow = document.getElementById("input-row");
 let inputCol = document.getElementById("input-col");
 let inputButton2 = document.getElementById("button-create-table");
 let outputTable = document.getElementById("table");
+let outputBr = document.getElementsByTagName("br");
 
 function modifyId(){
     let myId = Number(620610786);
     outputResult1.innerHTML = myId + Number(inputNum.value);
 }
+function createTable(){
+    modifyId();
+    let myName = "Nattapon Tancho" + String(myId + Number(inputNum.value));
+    outputBr.innerHTML = myName;
+
+}
 inputButton1.addEventListener('click',modifyId);
+inputButton2.addEventListener('click',createTable);
