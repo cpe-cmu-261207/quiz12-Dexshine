a0fc6582329be1cfad9f7ff3d3edbb032ef615b0
Dexshine <b_o_y_s@hotmail.com>
2021-01-12 13:52:44 +0700

    getElementById

--- app_template.js
+++ app_template.js
@@ -1,10 +1,15 @@
 //your code here!
 document.addEventListener('DOMContentloaded',modifyId);
 let inputNum = document.getElementById("input-number");
-let outputButton1 = document.getElementById("button-compute");
+let inputButton1 = document.getElementById("button-compute");
 let outputResult1 = document.getElementById("compute-result");
+let inputRow = document.getElementById("input-row");
+let inputCol = document.getElementById("input-col");
+let inputButton2 = document.getElementById("button-create-table");
+let outputTable = document.getElementById("table");
+
 function modifyId(){
     let myId = Number(620610786);
     outputResult1.innerHTML = myId + Number(inputNum.value);
 }
-outputButton1.addEventListener('click',modifyId);
+inputButton1.addEventListener('click',modifyId);
