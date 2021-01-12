4cf3708c2b8944809150f7b73ca8d451035ce774
Dexshine <b_o_y_s@hotmail.com>
2021-01-12 13:46:29 +0700

    modifyID

--- app_template.js
+++ app_template.js
@@ -1,1 +1,10 @@
 //your code here!
+document.addEventListener('DOMContentloaded',modifyId);
+let inputNum = document.getElementById("input-number");
+let outputButton1 = document.getElementById("button-compute");
+let outputResult1 = document.getElementById("compute-result");
+function modifyId(){
+    let myId = Number(620610786);
+    outputResult1.innerHTML = myId + Number(inputNum.value);
+}
+outputButton1.addEventListener('click',modifyId);
